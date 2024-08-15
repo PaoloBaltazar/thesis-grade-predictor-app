01432f5dd5673510999191dc10f70f8ef0a66975
PaoloBaltazar <gpaolobaltazar@gmail.com>
2024-08-15 22:46:41 +0800

    add columns for prediction data

--- website/db_model.py
+++ website/db_model.py
@@ -2,6 +2,13 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+class Data(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  attendance = db.Column(db.Integer)
+  previousGrade = db.Column(db.Integer)
+  financialSituation = db.Column(db.Integer)
+  learningEnvironment = db.Column(db.Integer)
+
 class User(db.Model, UserMixin):
   id = db.Column(db.Integer, primary_key=True)
   email = db.Column(db.String(150), unique=True)
