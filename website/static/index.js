75394247de66bd8cf448cd716d87172898f5a998
PaoloBaltazar <gpaolobaltazar@gmail.com>
2024-08-17 21:12:55 +0800

    implement tables to show model data

--- website/static/index.js
+++ website/static/index.js
@@ -1,29 +1,3 @@
-document.getElementById('prediction-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-
-  const attendance = document.getElementsByName('attendance')[0].value;
-  const financialSituation = document.getElementsByName('financial_situation')[0].value;
-  const learningEnvironment = document.getElementsByName('learning_environment')[0].value;
-  const previousGrades = document.getElementsByName('previous_grades')[0].value;
-
-  const data = {
-      features: [attendance, financialSituation, learningEnvironment, previousGrades]
-  };
-
-  fetch('/predict', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-  })
-  .then(response => response.json())
-  .then(data => {
-      document.getElementById('prediction-result').innerText = 'Predicted Grade: ' + data.prediction;
-  })
-  .catch(error => console.error('Error:', error));
-});
-
 const toggleBtn = document.getElementById('toggle-btn');
 const toggleBtnCollapsed = document.getElementById('toggle-btn-collapsed');
 const sidebar = document.getElementById('sidebar');
@@ -40,3 +14,43 @@ sidebar.classList.remove('collapsed');
 toggleBtnCollapsed.style.display = 'none';
 toggleBtn.style.display = 'block';
 });
+
+function updateSliderValue(sliderId, valueId) {
+    const slider = document.getElementById(sliderId);
+    const valueDisplay = document.getElementById(valueId);
+
+    slider.addEventListener('input', () => {
+      valueDisplay.textContent = slider.value;
+    });
+  }
+
+// Initialize the sliders
+updateSliderValue('financialSituation', 'financialSituationValue');
+updateSliderValue('learningEnvironment', 'learningEnvironmentValue');
+
+document.getElementById('prediction-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  const attendance = document.getElementsByName('attendance')[0].value;
+  const previousGrades = document.getElementsByName('previous_grades')[0].value;
+  const financialSituation = document.getElementsByName('financial_situation')[0].value;
+  const learningEnvironment = document.getElementsByName('learning_environment')[0].value;
+
+  const data = {
+      features: [attendance, previousGrades, financialSituation, learningEnvironment]
+  };
+
+  fetch('/predict', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(data => {
+      document.getElementById('prediction-result').innerText = 'Predicted Grade: ' + data.prediction;
+  })
+  .catch(error => console.error('Error:', error));
+});
+
--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, send_from_directory, jsonify
 import joblib
 import numpy as np
+import csv
 
 views = Blueprint('views', __name__)
 
@@ -9,7 +10,8 @@ scaler = joblib.load('scaler.pkl')
 
 @views.route('/')
 def home():
-  return render_template("home.html")
+  csv_data = parse_csv('data_prevgrade.csv')
+  return render_template("home.html", csv_data=csv_data)
 
 @views.route('/predict', methods=['POST'])
 def predict():
@@ -18,3 +20,10 @@ def predict():
     features = scaler.transform(features)
     prediction = model.predict(features)
     return jsonify({'prediction': prediction.tolist()})
+
+
+def parse_csv(filepath):
+   with open(filepath, mode='r', encoding='utf-8-sig') as csv_file:
+      reader = csv.reader(csv_file)
+      csv_data = [row for row in reader]
+      return csv_data
