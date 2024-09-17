57f07a4f52d4e7257d4b4120a05efd14e2e05748
PaoloBaltazar <gpaolobaltazar@gmail.com>
2024-09-18 03:59:06 +0800

    last chance

--- website/__init__.py
+++ website/__init__.py
@@ -7,42 +7,42 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
-
 def create_app():
-  app = Flask(__name__)
-  app.config['SECRET_KEY'] = 'thesisgradepredictorapp'
-  app.config['SQLALCHEMY_DATABASE_URI']  = os.getenv('DATABASE_URL', 'postgres://thesis_database_user:77GHKgFXEYDY0atVLc0APBxUhkMJmOSR@dpg-crktbtrv2p9s7e7cgm0-a:5432/thesis_database')
-  db.init_app(app)
+    app = Flask(__name__)
+    
+    # Securely load sensitive configurations
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'fallback-secret-key')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///default.db')
+    
+    db.init_app(app)
 
-  from .views import views
-  from .auth import auth
+    # Register blueprints
+    from .views import views
+    from .auth import auth
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
-  app.register_blueprint(views, url_prefix='/')
-  app.register_blueprint(auth, url_prefix='/')
+    # Create database and add initial users
+    from .db_model import User
+    from .initial_users import add_initial_users
 
-  from .db_model import User
-  from .initial_users import add_initial_users
+    create_database(app)
+    add_initial_users(app)
 
-  create_database(app)
-  add_initial_users(app)
+    # Login management
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
 
-  login_manager = LoginManager()
-  login_manager.login_view = 'auth.login'
-  login_manager.init_app(app)
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
-
-  #Telling flask how we load user and look for the primary key
-  @login_manager.user_loader
-  def load_user(id):
-    return User.query.get(int(id))
-
-  return app
+    return app
 
 def create_database(app):
-  if not path.exists('website/' + DB_NAME):
-    with app.app_context():
-      db.create_all()
-    print('Created Database!')
-
-
-
+    # Only useful for SQLite in local development
+    if not path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+        print('Created Database!')
