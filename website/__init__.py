bf32b41269f343b6a75d8fe2b8fa669260f6de22
PaoloBaltazar <gpaolobaltazar@gmail.com>
2024-09-18 12:32:26 +0800

    increase password length

--- website/__init__.py
+++ website/__init__.py
@@ -3,8 +3,10 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+migrate = Migrate()  # Create an instance of Migrate
 
 def create_app():
     app = Flask(__name__)
@@ -16,6 +18,7 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+    migrate.init_app(app, db)  # Initialize Flask-Migrate with the app and database
 
     # Register blueprints
     from .views import views
